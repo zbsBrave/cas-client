62de0199d565941525818cf886b8b5bae37e6aff
zhangbaisen <zhangbaisen@xy51.com>
2022-04-02 16:41:18 +0800

    cas configurer注释

---
+++ src/main/java/zbs/casclient/config/cas/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ * cas使用
+ *      1，CasProperties加入容器
+ *      2，配置CasConfigurer： http.apply(new CasConfigurer<>())
+ * 
+ * @author zbs
+ * @since 2022/4/2 16:39
+ */
+package zbs.casclient.config.cas;
