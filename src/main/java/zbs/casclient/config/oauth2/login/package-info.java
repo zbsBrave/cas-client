3aa46d27937c2993913618e2fde01189b064f825
zhangbaisen <zhangbaisen@xy51.com>
2022-04-19 18:32:27 +0800

    oauth登录

---
+++ src/main/java/zbs/casclient/config/oauth2/login/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * @author zbs
+ * @since 2022/4/19 18:15
+ * https://docs.spring.io/spring-security/reference/servlet/oauth2/login/index.html
+ * 功能：
+ *      提供了让用户使用其在 OAuth 2.0 提供者（例如 GitHub）或 OpenID Connect 1.0 提供者（例如 Google）处的现有帐户登录应用程序的能力。
+ *      provides an application with the capability to have users log in to the application by using their existing account at an 
+ *      OAuth 2.0 Provider (e.g. GitHub) or OpenID Connect 1.0 Provider (such as Google).
+ * 
+ * demo:
+ *      https://github.com/spring-projects/spring-security-samples/tree/5.6.x/servlet/spring-boot/java/oauth2/login
+ * 
+ * 
+ * 
+ */
+package zbs.casclient.config.oauth2.login;
