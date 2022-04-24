edd3f7c204c6b6c34b89e981c9b340efbf18bc4b
zhangbaisen <zhangbaisen@xy51.com>
2022-04-24 10:27:33 +0800

    说明

---
+++ src/main/java/zbs/casclient/config/oauth2/client/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * @author zbs
+ * @since 2022/4/19 17:27
+ * 
+ * 需要引入：
+ *      1，spring-boot-starter-security
+ *      2，spring-boot-starter-oauth2-client
+ * CommonOAuth2Provider
+ * ClientAuthenticationMethod
+ * 
+ */
+package zbs.casclient.config.oauth2.client;
--- src/main/java/zbs/casclient/config/oauth2/resource/package-info.java
+++ src/main/java/zbs/casclient/config/oauth2/resource/package-info.java
@@ -1,16 +1,34 @@
 /**
  * @author zbs
  * @since 2022/4/6
- * 
+ *
  *  资源服务器
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
+ *
+ * 需要引入：
+ *      1，spring-boot-starter-security
+ *      2，spring-boot-starter-oauth2-resource-server
+ * 配置：
+ *      
+ *      spring.security.oauth2.resourceserver.jwt.issuer-uri
+ *      spring.security.oauth2.resourceserver.jwt.jwk-set-uri
+ *
+
+BearerTokenAuthenticationFilter
+Authorization
+access_token
+
+JwtAuthenticationProvider
+
+    jwkSetUri 一般是授权服务器提供的获取JWK配置的well-known端点，用来校验JWT Token。
+    jwsAlgorithm 指定jwt使用的算法，默认 RSA-256。
+    issuerUri 获取OAuth2.0 授权服务器元数据的端点。
+    publicKeyLocation 用于解码的公钥路径，作为资源服务器来说将只能持有公钥，不应该持有私钥
+
+BearerTokenAuthenticationFilter
+JwtAuthenticationToken
+
+NimbusJwtDecoder.withJwkSetUri(uri).build();
+ 
+ 
  */
 package zbs.casclient.config.oauth2.resource;
