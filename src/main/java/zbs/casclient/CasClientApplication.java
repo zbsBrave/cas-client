4bd46afa35ab64293d1898b2c2aa5698734dd81f
zhangbaisen <zhangbaisen@xy51.com>
2022-04-29 11:36:49 +0800

    generator other

--- src/main/java/zbs/casclient/CasClientApplication.java
+++ src/main/java/zbs/casclient/CasClientApplication.java
@@ -6,6 +6,8 @@ import com.baomidou.mybatisplus.generator.config.converts.MySqlTypeConvert;
 import com.baomidou.mybatisplus.generator.config.rules.DateType;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Consumer;
 
 /**
@@ -27,7 +29,7 @@ public class CasClientApplication {
 
     private static final String username = "syhd";
     private static final String pwd = "syhd!@#321";
-    
+
     public static void main(String[] args) {
         FastAutoGenerator.create(getDataSourceConfig())
                 .globalConfig(getGlobalConfig())
@@ -43,14 +45,21 @@ public class CasClientApplication {
                             .mapperBuilder().fileOverride().enableBaseColumnList().enableBaseResultMap();
                 })
                 .templateConfig(getTemplateConfig())
+                .injectionConfig(getInjectionConfig()) //注入自定义配置
                 .execute();
     }
-    
-    public static DataSourceConfig.Builder getDataSourceConfig(){
+
+    /**
+     * 数据库配置
+     */
+    public static DataSourceConfig.Builder getDataSourceConfig() {
         return new DataSourceConfig.Builder(url_user, username, pwd)
                 .typeConvert(new MySqlTypeConvert());
     }
 
+    /**
+     * 全局配置
+     */
     public static Consumer<GlobalConfig.Builder> getGlobalConfig() {
         return builder -> builder.author("zbs") // 设置作者
 //                .enableSwagger() // 开启 swagger 模式
@@ -59,20 +68,46 @@ public class CasClientApplication {
                 .outputDir(FILE_PATH_JAVA); // 指定输出目录
     }
 
+    /**
+     * 包配置
+     */
     public static Consumer<PackageConfig.Builder> getPackageConfig() {
+        Map<OutputFile, String> map = new HashMap<>();
+        map.put(OutputFile.xml,FILE_PATH_XML);
+        map.put(OutputFile.other,FILE_PATH_XML);
         return builder -> builder.parent("com.syhd") // 设置父包名
                 .entity("model")
                 .service("service")
                 .service("service.impl")
                 .mapper("mapper")
                 .xml("mapper.xml")
-                .pathInfo(Collections.singletonMap(OutputFile.xml, FILE_PATH_XML)); // 设置mapperXml生成路径
+//                .pathInfo(Collections.singletonMap(OutputFile.xml, FILE_PATH_XML)) // 设置mapperXml生成路径
+                .pathInfo(map) // 设置mapperXml生成路径
+                ;
     }
 
+    /**
+     * 模板配置
+     */
     public static Consumer<TemplateConfig.Builder> getTemplateConfig() {
         return builder -> builder.xml(TEMPLATE_XML)
                 .mapper(TEMPLATE_MAPPER)
                 .entity(TEMPLATE_ENTITY)
+                .disable(TemplateType.CONTROLLER) //禁用模板
+                ;
+    }
+
+    /**
+     * 注入自定义配置
+     */
+    public static Consumer<InjectionConfig.Builder> getInjectionConfig() {
+        return builder -> builder.beforeOutputFile((tableInfo, objectMap) -> {
+                    System.out.println("tableInfo: " + tableInfo.getEntityName() + " objectMap: " + objectMap.size());
+                })
+                //自定义配置 Map 对象。例如以下配置，可以在模板文件中通过key=testKey，获取对应的值=testVal
+                .customMap(Collections.singletonMap("testKey", "testVal"))
+                //自定义配置模板文件
+                .customFile(Collections.singletonMap("other.java", "template-mybatis/other.java.vm"))
                 ;
     }
 }
