bc778702d070655b246bed3ca81f7e601759d578
zhangbaisen <zhangbaisen@xy51.com>
2022-09-07 11:35:14 +0800

    插入排序

---
+++ src/main/java/zbs/casclient/algorithm/A3_InsertionSort.java
@@ -0,0 +1,26 @@
+package zbs.casclient.algorithm;
+
+import java.util.Arrays;
+
+/**
+ * @author zbs
+ * @since 2022/9/7 11:30
+ */
+public class A3_InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {1,2,2,5,6,7,7,9,354,6,0,545,32};
+        insertionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    
+    public static void insertionSort(int[] arr){
+        for(int p = 1; p < arr.length; p++){
+            int temp = arr[p];
+            int i = p;
+            for(; i > 0 && arr[i-1] > temp; i--){
+                arr[i] = arr[i-1];
+            }
+            arr[i] = temp;
+        }
+    }
+}
