8057fc70e1c5ffd5a5ec8b98747d8b12a5865fc8
zhangbaisen <zhangbaisen@xy51.com>
2022-09-15 17:20:10 +0800

    01背包->分割等和子集

--- src/main/java/zbs/casclient/algorithm/code/Dp2_416.java
+++ src/main/java/zbs/casclient/algorithm/code/Dp2_416.java
@@ -1,6 +1,7 @@
 package zbs.casclient.algorithm.code;
 
 /**
+ * 分割等和子集
  * @author zbs
  * @since 2022/9/14 16:56
  */
@@ -11,40 +12,39 @@ public class Dp2_416 {
         // 不放入背包 = dp[i-1,j]
         // 放入背包   = dp[i-1, j- i的容量] + i的价值
         //dp[i,j] = Math.max(dp[i-1,j], dp[i-1, j - i的容量] + i的价值);
-        
+
         int[] nums = new int[]{2, 2, 3, 7};
         System.out.println(canPartition(nums));
     }
 
 
     public static boolean canPartition(int[] nums) {
-        if(nums.length < 2){
+        if (nums.length < 2) {
             return false;
         }
         int sum = 0;
-        for(int n : nums){
+        for (int n : nums) {
             sum += n;
         }
-        if((sum & 1) == 1){
+        if ((sum & 1) == 1) {
             //奇数时肯定不能分割成两个元素和相等的子集
             return false;
         }
-        //背包的容量=sum/2，物品的个数=nums.length
-        boolean[][] dp = new boolean[nums.length][sum/2 + 1];
-        for(int i=1; i < nums.length; i++){
-            for(int j=0; j <= sum/2; j++){
-                if(j == nums[i]){
+        int len = nums.length;
+        int size = sum / 2;
+        boolean[][] dp = new boolean[len + 1][size + 1];
+        for (int i = 1; i <= len; i++) {
+            for (int j = 0; j <= size; j++) {
+                if (j == nums[i - 1]) {
                     //第i个物品刚好能装下
-                    dp[i][j] = true;
-                } else if(j > nums[i]){
-                    //第i个物品<容量
-                    dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i]];
-                }else {
-                    dp[i][j] = dp[i-1][j];
+                    dp[i][j] = true;//也可以不判断相等的情况，但是要初始化 dp[0][0] = true
+                } else if (j >= nums[i - 1]) {
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
                 }
             }
         }
-        
-        return dp[nums.length-1][sum/2];
+        return dp[len][size];
     }
 }
